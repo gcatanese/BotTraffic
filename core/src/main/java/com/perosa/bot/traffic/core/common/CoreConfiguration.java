63a16a04bd5b706422685ce4edf761a1eb4d1993
gcatanese <gcatanese@yahoo.com>
2019-08-20 15:50:36 +0200

    Add new prop

--- core/src/main/java/com/perosa/bot/traffic/core/common/CoreConfiguration.java
+++ core/src/main/java/com/perosa/bot/traffic/core/common/CoreConfiguration.java
@@ -10,12 +10,12 @@ public class CoreConfiguration {
     public String getHome() {
         String home = System.getProperty("bt.home");
 
-        if(home == null || home.isEmpty()) {
-            throw new RuntimeException("bt.home is undefined");
-        }
-
-        if(!home.endsWith("/")) {
-            home = home + "/";
+        if (home == null || home.isEmpty()) {
+            home = "";
+        } else {
+            if (!home.endsWith("/")) {
+                home = home + "/";
+            }
         }
 
         return home;
@@ -24,12 +24,21 @@ public class CoreConfiguration {
     public int getPort() {
         String port = System.getProperty("bt.port");
 
-        if(port == null || port.isEmpty()) {
-            port = "8081";
+        if (port == null || port.isEmpty()) {
+            port = "8886";
         }
 
         return Integer.valueOf(port);
     }
 
+    public int getMetricsHandlerPort() {
+        String port = System.getProperty("bt.metricsport");
+
+        if (port == null || port.isEmpty()) {
+            port = "8887";
+        }
+
+        return Integer.valueOf(port);
+    }
 
 }
