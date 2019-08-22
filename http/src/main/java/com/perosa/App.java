b10f642f1777c7979df9619e1fe573ae20780d2e
gcatanese <gcatanese@yahoo.com>
2019-08-22 12:23:46 +0200

    Start folder watching

--- http/src/main/java/com/perosa/App.java
+++ http/src/main/java/com/perosa/App.java
@@ -1,6 +1,8 @@
 package com.perosa;
 
 
+import com.perosa.bot.traffic.core.rule.registry.RuleRegistryWatcher;
+import com.perosa.bot.traffic.core.service.registry.ServiceRegistryWatcher;
 import com.perosa.bot.traffic.http.metrics.prometheus.MetricsHandler;
 import com.perosa.bot.traffic.http.server.ReverseProxy;
 import io.prometheus.client.hotspot.DefaultExports;
@@ -20,6 +22,9 @@ public class App {
 
         new ReverseProxy().setUp();
         new MetricsHandler().setUp();
+
+        new RuleRegistryWatcher().init();
+        new ServiceRegistryWatcher().init();
 
         LOGGER.info("App is up!");
 
