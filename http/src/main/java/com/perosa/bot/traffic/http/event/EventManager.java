9eaedd91c921391c278e3da7bd521f7f5ad6c0b7
perosa <beppe.catanese@gmail.com>
2019-10-29 14:09:43 +0100

    Remove mbassadir

--- http/src/main/java/com/perosa/bot/traffic/http/event/EventManager.java
+++ http/src/main/java/com/perosa/bot/traffic/http/event/EventManager.java
@@ -1,10 +1,7 @@
 package com.perosa.bot.traffic.http.event;
 
-import net.engio.mbassy.bus.MBassador;
-import net.engio.mbassy.bus.config.BusConfiguration;
-import net.engio.mbassy.bus.config.Feature;
-import net.engio.mbassy.bus.config.IBusConfiguration;
-import net.engio.mbassy.bus.error.IPublicationErrorHandler;
+import com.perosa.bot.traffic.http.event.type.prometheus.PrometheusEvent;
+import com.perosa.bot.traffic.http.event.type.prometheus.PrometheusEventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,28 +9,9 @@ public class EventManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EventManager.class);
 
-    private static MBassador bus;
-
-    static {
-        start();
-    }
-
-    private static void start() {
-
-        bus = new MBassador(new BusConfiguration()
-                .addFeature(Feature.SyncPubSub.Default())
-                .addFeature(Feature.AsynchronousHandlerInvocation.Default())
-                .addFeature(Feature.AsynchronousMessageDispatch.Default())
-                .addPublicationErrorHandler(new IPublicationErrorHandler.ConsoleLogger())
-                .setProperty(IBusConfiguration.Properties.BusId, "global bus"));
-
-        bus.subscribe(new EventListener());
-
-    }
-
     public static void sendEvent(Event event) {
         LOGGER.debug("sendEvent " + event);
-        bus.post(event).asynchronously();
+        new PrometheusEventHandler().process((PrometheusEvent)event);
     }
 }
 
--- http/src/test/java/com/perosa/bot/traffic/http/event/EventManagerTest.java
+++ http/src/test/java/com/perosa/bot/traffic/http/event/EventManagerTest.java
@@ -1,12 +1,13 @@
 package com.perosa.bot.traffic.http.event;
 
+import com.perosa.bot.traffic.http.event.type.prometheus.PrometheusEvent;
 import org.junit.jupiter.api.Test;
 
 public class EventManagerTest {
 
     @Test
     public void test() {
-        Event event = new Event("test");
+        Event event = new PrometheusEvent();
         EventManager.sendEvent(event);
     }
 }
