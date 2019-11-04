917946345e03090c622b190987bc2e9028c2ede9
perosa <beppe.catanese@gmail.com>
2019-11-04 12:42:02 +0100

    Implement shadowing workflow

---
+++ http/src/main/java/com/perosa/bot/traffic/http/server/workflow/Shadower.java
@@ -0,0 +1,53 @@
+package com.perosa.bot.traffic.http.server.workflow;
+
+import com.perosa.bot.traffic.http.client.Forwarder;
+import com.perosa.bot.traffic.http.client.ForwarderResponse;
+import com.perosa.bot.traffic.http.client.JavaClientImpl;
+import com.perosa.bot.traffic.http.client.wrap.Get;
+import com.perosa.bot.traffic.http.client.wrap.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+public class Shadower {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Shadower.class);
+
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    private Forwarder forwarder = new JavaClientImpl();
+
+    public void get(Get input) throws Exception {
+        push(callableGet(input));
+    }
+
+    public void post(Post input) throws Exception {
+        push(callablePost(input));
+    }
+
+    void push(Callable<String> task) {
+        Future<String> future = executor.submit(task);
+    }
+
+    Callable<String> callableGet(Get input) {
+
+        Callable<String> callable = () -> {
+            ForwarderResponse forwarderResponse = forwarder.get(input);
+            return "Pushed Get";
+        };
+
+        return callable;
+    }
+
+    Callable<String> callablePost(Post input) {
+
+        Callable<String> callable = () -> {
+            ForwarderResponse forwarderResponse = forwarder.post(input);
+            return "Pushed Post";
+        };
+
+        return callable;
+    }
+
+}
