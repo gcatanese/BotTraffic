d268639a0daa7bffc0a7483afca8b7ddeb576073
perosa <beppe.catanese@gmail.com>
2019-11-11 15:39:06 +0100

    Add Redis

--- core/src/main/java/com/perosa/bot/traffic/core/rule/registry/storage/RuleRegistryStorage.java
+++ core/src/main/java/com/perosa/bot/traffic/core/rule/registry/storage/RuleRegistryStorage.java
@@ -1,7 +1,10 @@
 package com.perosa.bot.traffic.core.rule.registry.storage;
 
+import com.perosa.bot.traffic.core.common.EnvConfiguration;
 import com.perosa.bot.traffic.core.rule.Rule;
 import com.perosa.bot.traffic.core.rule.registry.storage.file.FileRuleRegistry;
+import com.perosa.bot.traffic.core.rule.registry.storage.redis.RedisRuleRegistry;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -10,6 +13,15 @@ public interface RuleRegistryStorage {
     List<Rule> load();
 
     static RuleRegistryStorage make() {
-        return new FileRuleRegistry();
+
+        String storage = new EnvConfiguration().getStorage();
+        String redisHost = new EnvConfiguration().getRedisHost();
+        int redisPort = new EnvConfiguration().getRedisPort();
+
+        if (storage.equalsIgnoreCase("redis")) {
+            return new RedisRuleRegistry(new Jedis(redisHost, redisPort));
+        } else {
+            return new FileRuleRegistry();
+        }
     }
 }
